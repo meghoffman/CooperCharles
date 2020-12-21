c4c7ca37a22d61801c77971abbb7aa4dc2f5cf12
meghoffman <meg.hoffman@verizon.net>
2020-12-21 10:26:50 -0500

    Add Cooper files

---
+++ NewCooper/script.js
@@ -0,0 +1,69 @@
+let birthNotice = document.getElementById("birth")
+let weightNotice = document.getElementById("weight")
+let lengthNotice = document.getElementById("length")
+let timeNotice = document.getElementById("time")
+let parentNotice = document.getElementById("parents")
+let friendNotice = document.getElementById("friends")
+
+function birth(){
+    birthNotice.innerHTML = "November 2, 2020"
+    birthNotice.style.color = "#57c7de"    
+}
+
+function birthout(){
+  birthNotice.innerHTML = "date of birth"
+  birthNotice.style.color = "black"
+}
+
+function weight(){
+    weightNotice.innerHTML = "7 pounds 11 ounces"
+    weightNotice.style.color = "#57c7de"
+}
+
+function weightout(){
+    weightNotice.innerHTML = "weight"
+    weightNotice.style.color = "black"
+}
+
+function length(){
+    lengthNotice.innerHTML = "21 inches"
+    lengthNotice.style.color = "#57c7de"
+}
+
+function lengthout(){
+    lengthNotice.innerHTML = "length"
+    lengthNotice.style.color = "black"
+}
+
+function time(){
+  timeNotice.innerHTML = "12:51 AM"
+  timeNotice.style.color = "#57c7de"
+}
+
+function timeout(){
+  timeNotice.innerHTML = "time of birth"
+  timeNotice.style.color = "black"
+}
+
+function parents(){
+  parentNotice.innerHTML = "suzanne & meaghan"
+  parentNotice.style.color = "#57c7de"
+}
+
+function parentsout(){
+  parentNotice.innerHTML = "parents"
+  parentNotice.style.color = "black"
+}
+
+function friends(){
+  friendNotice.innerHTML = "google, cupcake, boogie"
+  friendNotice.style.color = "#57c7de"
+  friendNotice.style.fontSize = "15px"
+}
+
+function friendsout(){
+  friendNotice.innerHTML = "friends"
+  friendNotice.style.color = "black"
+  friendNotice.style.fontSize = "20px"
+}
+
